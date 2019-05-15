ca05331ed9944341f4f11760d08946405fae6eee
C.C <34416450+C-mirrors@users.noreply.github.com>
2019-05-15 19:44:39 +0800

    Add files via upload

--- Smartdoc.py
+++ Smartdoc.py
@@ -1,6 +1,22 @@
 import sys
 import webbrowser
 
+def fileToHtml(filename):
+    #通过输入文件生成html
+    file = open(filename, encoding='utf8')
+    text = file.read()
+    file.close()
+    text = escape(text)
+    if filename[-1] == 't':  # 若是srs文件，则添加到code的链接
+        text = txtTopy(text);
+    if filename[-1] == 'y':  # 若是code文件，则添加到srs的链接
+        text = pyTotxt(text)
+    text = preChange(text)
+    file = open(filename + '_htmlpage.html', 'wt', encoding='utf8')
+    file.write(text)
+    file.close()
+    webbrowser.open(filename + '_htmlpage.html', new=1)
+
 def escape(text):
     # 将text文本中的空格、&、<、>、（"）、（'）转化成对应的的字符实体，以方便在html上显示
     text = text.replace('&', '&#38;')
@@ -11,7 +27,7 @@ def escape(text):
     text = text.replace('\'', '&#39;')
     return text
 
-def changetohtml(text):
+def preChange(text):
     #将text以行为单位加上<li></li>标签
     lines = text.split('\n')
     i = 0
@@ -21,18 +37,39 @@ def changetohtml(text):
     text = ''.join(lines)
     return text
 
+def txtTopy(text):
+    #向srs文件中添加到code的链接
+    lines = text.split('@')
+    i = 0
+    for line in lines:
+        if line != '':
+            lines[i] = '<a href="code.py_htmlpage.html#RQ' + str(i) + '" id="RQ' + str(i) + '">#{see function}' + '</a>\n@' + line
+        i += 1
+    text = ''.join(lines)
+    return text
+
+def pyTotxt(text):
+    #向code文件中添加到srs的链接
+    lines = text.split('#{see')
+    i = 0
+    for line in lines:
+        if len(lines[i]) > 3:
+            if line[8].isdigit():
+                lines[i] = '<a href="srs.txt_htmlpage.html#RQ' + line[8] + '" id="RQ' + line[8] + \
+                           '">#{see RQ' + line[8] + '}' + '</a>\n' + line[line.find('\n')+1:]
+            else:
+                lines[i] = '<a href="code.py_htmlpage.html#RQ" id="RQ">#{see RQ}' \
+                           + ' # No link to srs</a>\n' + line[line.find('\n')+1:]
+        i += 1
+    text = ''.join(lines)
+    return text
 
 try:
-    filename = input('Please enter the file name:')
-    filename = filename.replace('\r', '')  # 在控制台中输入回车后文件名会多一个'\r'，需要去掉
-    f = open(filename, encoding='utf8')
-    t = f.read()
-    f.close()
-    t = escape(t)
-    t = changetohtml(t)
-    f = open(filename + '_htmlpage.html', 'wt', encoding='utf8')
-    f.write(t)
-    f.close()
-    webbrowser.open(filename + '_htmlpage.html', new=1)
+    s = input('Please enter the file name(e.g.: srs.txt code.py):')
+    filename1, filename2 = s.split()
+    print(filename1, filename2)
+    fileToHtml(filename1)
+    fileToHtml(filename2)
+
 except:
     print("Unexpected error:", sys.exc_info()[0], sys.exc_info()[1])
--- code.py
+++ code.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2019 Hui Lan
+#{see RQ1}
 def file2lst(fname):
     ''' Return a list where each element is a word from fname. '''
     L = []
@@ -11,7 +11,7 @@ def file2lst(fname):
     f.close()
     return L
 
-
+#{see RQ2}
 def lst2dict(lst):
     ''' Return a dictionary given list lst.  Each key is an element in the lst.
     The value is always 1.'''
@@ -20,14 +20,13 @@ def lst2dict(lst):
         d[w] = 1 
     return d
 
-
-
 def word_frequency(fname, english_dictionary):
+    #{see RQ3}
     ''' Return a dictionary where each key is a word both in the file fname and in 
     the dictionary english_dictionary, and the corresponding value is the frequency
     of that word.'''
     d = {}
-    L = file2lst(fname)
+    L = file2lst(fname) #{see RQ}
     for x in L:
         x = x.lower()
         if x in english_dictionary:
@@ -37,14 +36,12 @@ def word_frequency(fname, english_dictionary):
                 d[x] += 1
     return d
 
-
 def sort_by_value(d):
     ''' Return a sorted list of tuples, each tuple containing a key and a value.
         Note that the tuples are order in descending order of the value.'''
-    import operator
+    import operator #{see RQ4}
     lst = sorted(d.items(), key=operator.itemgetter(1), reverse=True)    
     return lst
-
 
 if __name__ == '__main__':    
     ed = lst2dict(file2lst('words.txt')) # from http://greenteapress.com/thinkpython2/code/words.txt
